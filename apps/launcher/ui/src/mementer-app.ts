e00e8c44571c03fa7a2ea52004801889d84b2dfa
jhweir <james@weco.io>
2022-10-27 22:43:01 +0100

    Expandable mementer circles set up

--- apps/launcher/ui/src/mementer-app.ts
+++ apps/launcher/ui/src/mementer-app.ts
@@ -3,6 +3,9 @@ import { property, state } from 'lit/decorators.js';
 import { AdminWebsocket, AppWebsocket, InstalledCell } from '@holochain/client';
 import { HolochainClient, CellClient } from '@holochain-open-dev/cell-client';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+import * as d3 from 'd3';
+
+type sizes = 'default' | 'small' | 'medium' | 'large'
   
 export class MementerApp extends ScopedElementsMixin(LitElement) {
     @state()
@@ -29,10 +32,65 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
         ) as InstalledCell;
     
         const cellClient = new CellClient(client, mementerCell);
+
+        this._loading = false;
     }
+
     async firstUpdated() {
         await this.connectToHolochain();
     }
+
+    
+
+    updated() {
+      // build mementer
+      const t = this
+      const canvas = t.shadowRoot?.getElementById('canvas')
+      const svg = d3.select(canvas!).append('svg').attr('width', 500).attr('height', 500)
+
+      function onMouseDown(size: sizes) {
+        const smallCircle = t.shadowRoot?.getElementById('small-circle')
+        const mediumCircle = t.shadowRoot?.getElementById('medium-circle')
+        const largeCircle = t.shadowRoot?.getElementById('large-circle')
+        const sizeValues = {
+          default: [50, 75, 100],
+          small: [80, 90, 100],
+          medium: [10, 90, 100],
+          large: [10,20,100]
+        }
+        d3.select(smallCircle!).transition().duration(1000).attr('r', sizeValues[size][0])
+        d3.select(mediumCircle!).transition().duration(1000).attr('r', sizeValues[size][1])
+        d3.select(largeCircle!).transition().duration(1000).attr('r', sizeValues[size][2])
+      }
+
+      svg
+          .append('rect')
+          .attr('id', 'background')
+          .attr('width', 500)
+          .attr('height', 500)
+          .attr('fill', 'white')
+          .style('cursor', 'pointer')
+          .on('mousedown', () => onMouseDown('default'))
+
+      function createCircle(size: sizes, radius: number) {
+        svg
+          .append('circle')
+          .attr('id', `${size}-circle`)
+          .attr('r', radius)
+          .attr('cx', 250)
+          .attr('cy', 250)
+          .attr('stroke', 'black')
+          .attr('strokeWidth', 5)
+          .attr('fill', 'white')
+          .style('cursor', 'pointer')
+          .on('mousedown', () => onMouseDown(size))
+      }
+
+      createCircle('large', 100)
+      createCircle('medium', 75)
+      createCircle('small', 50)
+    }
+
     render() {
         if (this._loading)
           return html`<div
@@ -42,8 +100,9 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
            Loading...
           </div>`;
     
-        return html`LOADED!`
+        return html`<div id='canvas' style="flex: 1; height: 100%; width: 100%; align-items: center; justify-content: center;"></div>`
     }
+
     static get scopedElements() {
         return {
         };

152d769d8a9710e06b620b33e5cf433c8f87240f
jhweir <james@weco.io>
2022-11-04 18:46:18 +0000

    Dynamic slice times, UI tweaks, code clean up

--- apps/launcher/ui/src/mementer-app.ts
+++ apps/launcher/ui/src/mementer-app.ts
@@ -16,30 +16,32 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
 
     circleSize = 500
 
-    circleColors = { small: '#ddd', medium: '#ccc', large: '#bbb' }
+    circleColors = { large: '#bbb', medium: '#ccc', small: '#ddd' }
 
-    timerColors = { small: 'red', medium: 'green', large: 'blue' }
+    timerColors = { large: 'blue', medium: 'green', small: 'red' }
 
     totalDuration = 6000 // seconds
 
-    numberOfSlices = { small: 6, medium: 12, large: 24 }
+    numberOfSlices = { large: 24, medium: 12, small: 6 }
 
     circleDurations = {
-      small: this.totalDuration / this.numberOfSlices.large / this.numberOfSlices.medium,
+      large: this.totalDuration,
       medium: this.totalDuration / this.numberOfSlices.large,
-      large: this.totalDuration
+      small: this.totalDuration / this.numberOfSlices.large / this.numberOfSlices.medium
     }
 
     focusStateScales = {
-      default: { small: 0.5, medium: 0.75, large: 1 },
-      small: { small: 0.8, medium: 0.9, large: 1 },
-      medium: { small: 0.1, medium: 0.9, large: 1 },
-      large: { small: 0.1, medium: 0.2, large: 1 },
+      default: { large: 1, medium: 0.75, small: 0.5 },
+      large: { large: 1, medium: 0.2, small: 0.1 },
+      medium: { large: 1, medium: 0.9, small: 0.1 },
+      small: { large: 1, medium: 0.9, small: 0.8 },
     }
 
     focusState: focusStates = 'default'
 
     timerActive: boolean = false
+
+    sizesArray: sizes[] = ['large', 'medium', 'small']
   
     async connectToHolochain() {
         const url = `ws://localhost:${process.env.HC_PORT}`
@@ -61,7 +63,9 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
 
     createSlices(size: sizes) {
       const group = d3.select(this.shadowRoot?.getElementById(`${size}-circle-group`)!)
+      // remove old slices
       group.selectAll('path').remove()
+      // create new slices
       for (let i = 0; i < this.numberOfSlices[size]; i += 1) {
         const arc = this.findArc(size, i, i + 1)
         group
@@ -88,28 +92,27 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
 
     updateFocusState(focus: focusStates) {
       this.focusState = focus
-      this.transitionCircleSize('small')
-      this.transitionCircleSize('medium')
-      this.transitionCircleSize('large')
+      this.sizesArray.forEach((size: sizes) => this.transitionCircleSize(size))
     }
 
     createCircle(svg: any, size: sizes) {
+      // create circle group
       svg
         .append('g')
         .attr('id', `${size}-circle-group`)
         .attr('transform', `translate(${this.svgSize / 2}, ${this.svgSize / 2})`)
         .style('cursor', 'pointer')
         .on('mousedown', () => this.updateFocusState(size))
-
+      // create slices
       this.createSlices(size)
     }
 
     createTimer(size: sizes, circleGroup?: any) {
       const group = circleGroup || d3.select(this.shadowRoot?.getElementById(`${size}-circle-group`)!)
       const arc = d3.arc().outerRadius(this.circleSize).innerRadius(0)
-      // remove old timer path
+      // remove old path
       group.select(`#${size}-timer`).remove()
-      // create new timer path
+      // create new path
       group
           .append('path')
           .attr('id', `${size}-timer`)
@@ -133,37 +136,56 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
           .on('end', () => this.createTimer(size, group))
     }
 
+    startTimer() {
+      const timerButton = this.shadowRoot?.getElementById('timer-button')
+      this.timerActive = true
+      timerButton!.textContent = 'Stop timer'
+      timerButton!.style.backgroundColor = '#ff8b8b'
+      this.sizesArray.forEach((size: sizes) => this.createTimer(size))
+    }
+
+    stopTimer() {
+      const timerButton = this.shadowRoot?.getElementById('timer-button')
+      this.timerActive = false
+      timerButton!.textContent = 'Start timer'
+      timerButton!.style.backgroundColor = '#8bc8ff'
+      this.sizesArray.forEach((size: sizes) => d3.select(this.shadowRoot?.getElementById(`${size}-timer`)!).interrupt('time').remove())
+    }
+
+    toggleTimer() {
+      if (this.timerActive) this.stopTimer()
+      else this.startTimer()
+    }
+
+    findCircleDurationText(size: sizes) {
+      return `(${+this.circleDurations[size].toFixed(2)}s / ${+(this.circleDurations[size] / this.numberOfSlices[size]).toFixed(2)}s)`
+    }
+
+    updateCircleDurations() {
+      // update durations
+      this.circleDurations = {
+        small: this.totalDuration / this.numberOfSlices.large / this.numberOfSlices.medium,
+        medium: this.totalDuration / this.numberOfSlices.large,
+        large: this.totalDuration
+      }
+      // update duration text
+      this.sizesArray.forEach((size: sizes) => {
+        const textElement = this.shadowRoot?.getElementById(`${size}-circle-durations`)
+        textElement!.textContent = this.findCircleDurationText(size)
+      })
+    }
+
     updateSlices(size: sizes, slices: number) {
+      this.stopTimer()
       this.numberOfSlices[size] = +slices
+      this.updateCircleDurations()
       this.createSlices(size)
     }
 
     updateTotalDuration(seconds: number) {
+      this.stopTimer()
       this.totalDuration = +seconds
-    }
-
-    toggleTimer() {
-      const timerButton = this.shadowRoot?.getElementById('timer-button')
-      if (this.timerActive) {
-        // stop timer
-        this.timerActive = false
-        timerButton!.textContent = 'Start timer'
-        d3.select(this.shadowRoot?.getElementById(`large-timer`)!).interrupt('time').remove()
-        d3.select(this.shadowRoot?.getElementById(`medium-timer`)!).interrupt('time').remove()
-        d3.select(this.shadowRoot?.getElementById(`small-timer`)!).interrupt('time').remove()
-      } else {
-        //start timer
-        this.timerActive = true
-        timerButton!.textContent = 'Stop timer'
-        this.circleDurations = {
-          small: this.totalDuration / this.numberOfSlices.large / this.numberOfSlices.medium,
-          medium: this.totalDuration / this.numberOfSlices.large,
-          large: this.totalDuration
-        }
-        this.createTimer('small')
-        this.createTimer('medium')
-        this.createTimer('large')
-      }
+      this.updateCircleDurations()
     }
 
     async firstUpdated() { await this.connectToHolochain() }
@@ -172,7 +194,6 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
       // create svg
       const canvas = this.shadowRoot?.getElementById('canvas')
       const svg = d3.select(canvas!).append('svg').attr('width', this.svgSize).attr('height', this.svgSize)
-
       // create background
       svg
           .append('rect')
@@ -182,83 +203,88 @@ export class MementerApp extends ScopedElementsMixin(LitElement) {
           .attr('fill', 'white')
           .style('cursor', 'pointer')
           .on('mousedown', () => this.updateFocusState('default'))
-  
       // create circle layers
-      this.createCircle(svg, 'large')
-      this.createCircle(svg, 'medium')
-      this.createCircle(svg, 'small')
+      this.sizesArray.forEach((size: sizes) => this.createCircle(svg, size))
     }
-
 
     render() {
-        if (this.loading)
-          return html`<div
-            class="row"
-            style="flex: 1; height: 100%; align-items: center; justify-content: center;"
-          >
-           Loading...
-          </div>`;
-    
-        return html`
-          <div style="display: flex; flex-direction: 'column'; height: 100%; width: 100%; align-items: center;">
-            <h1>The Mementer: The Chronogram of Life</h1>
-            <div style="display: flex; margin-bottom: 20px">
-            <div style="display: flex; align-items: center; margin-right: 20px">
-                <p style="margin: 0">Total duration (seconds)</p>
-                <input
-                  type='number'
-                  .value=${this.totalDuration}
-                  @keyup=${(e: any) => this.updateTotalDuration(e.target.value)}
-                  @change=${(e: any) => this.updateTotalDuration(e.target.value)}
-                  style="width: 100px; height: 30px; margin-left: 10px"
-                >
-              </div>
-              <div style="display: flex; align-items: center; margin-right: 20px">
-                <p style="margin: 0">Large slices</p>
-                <input
-                  type='number'
-                  .value=${this.numberOfSlices.large}
-                  @keyup=${(e: any) => this.updateSlices('large', e.target.value)}
-                  @change=${(e: any) => this.updateSlices('large', e.target.value)}
-                  style="width: 50px; height: 30px; margin-left: 10px"
-                >
-              </div>
-              <div style="display: flex; align-items: center; margin-right: 20px">
-                <p style="margin: 0">Medium slices</p>
-                <input
-                  type='number'
-                  .value=${this.numberOfSlices.medium}
-                  @keyup=${(e: any) => this.updateSlices('medium', e.target.value)}
-                  @change=${(e: any) => this.updateSlices('medium', e.target.value)}
-                  style="width: 50px; height: 30px; margin-left: 10px"
-                >
-              </div>
-              <div style="display: flex; align-items: center; margin-right: 20px">
-                <p style="margin: 0">Small slices</p>
-                <input
-                  type='number'
-                  .value=${this.numberOfSlices.small}
-                  @keyup=${(e: any) => this.updateSlices('small', e.target.value)}
-                  @change=${(e: any) => this.updateSlices('small', e.target.value)}
-                  style="width: 50px; height: 30px; margin-left: 10px"
-                >
-              </div>
-              <button
-                id='timer-button'
-                style="all: unset; background-color: #8bc8ff; padding: 0 10px; border-radius: 5px; cursor: pointer"
-                @click=${() => this.toggleTimer()}
-              >
-                Start timer
-              </button>
-            </div>
-            <div id='canvas'></div>
+      if (this.loading) return html`
+        <div
+          class="row"
+          style="flex: 1; height: 100%; align-items: center; justify-content: center;"
+        >
+          Loading...
+        </div>
+      `
+      return html`
+        <div style="display: flex; flex-direction: 'column'; height: 100%; width: 100%; align-items: center;">
+          <h1>The Mementer: The Chronogram of Life</h1>
+
+          <div style="display: flex; align-items: center; margin-bottom: 20px">
+            <p style="margin: 0">Total duration (seconds)</p>
+            <input
+              type='number'
+              .value=${this.totalDuration}
+              @keyup=${(e: any) => this.updateTotalDuration(e.target.value)}
+              @change=${(e: any) => this.updateTotalDuration(e.target.value)}
+              style="width: 100px; height: 30px; margin-left: 10px"
+            >
           </div>
-        `
+
+          <div style="display: flex; margin-bottom: 20px">
+            <div style="display: flex; align-items: center; margin-right: 20px">
+              <p style="margin: 0 10px 0 0">Large slices</p>
+              <input
+                type='number'
+                .value=${this.numberOfSlices.large}
+                @keyup=${(e: any) => this.updateSlices('large', e.target.value)}
+                @change=${(e: any) => this.updateSlices('large', e.target.value)}
+                style="width: 50px; height: 30px; margin-right: 10px"
+              >
+              <p id='large-circle-durations' style="margin: 0">
+                ${this.findCircleDurationText('large')}
+              </p>
+            </div>
+            <div style="display: flex; align-items: center; margin-right: 20px">
+              <p style="margin: 0 10px 0 0">Medium slices</p>
+              <input
+                type='number'
+                .value=${this.numberOfSlices.medium}
+                @keyup=${(e: any) => this.updateSlices('medium', e.target.value)}
+                @change=${(e: any) => this.updateSlices('medium', e.target.value)}
+                style="width: 50px; height: 30px; margin-right: 10px"
+              >
+              <p id='medium-circle-durations' style="margin: 0">
+                ${this.findCircleDurationText('medium')}
+              </p>
+            </div>
+            <div style="display: flex; align-items: center">
+              <p style="margin: 0 10px 0 0">Small slices</p>
+              <input
+                type='number'
+                .value=${this.numberOfSlices.small}
+                @keyup=${(e: any) => this.updateSlices('small', e.target.value)}
+                @change=${(e: any) => this.updateSlices('small', e.target.value)}
+                style="width: 50px; height: 30px; margin-right: 10px"
+              >
+              <p id='small-circle-durations' style="margin: 0">
+                ${this.findCircleDurationText('small')}
+              </p>
+            </div>
+          </div>
+
+          <button
+            id='timer-button'
+            style="all: unset; background-color: #8bc8ff; padding: 10px; border-radius: 5px; cursor: pointer; margin-bottom: 20px"
+            @click=${() => this.toggleTimer()}
+          >
+            Start timer
+          </button>
+          
+          <div id='canvas'></div>
+        </div>
+      `
     }
 
-    static get scopedElements() {
-        return {
-        };
-      }
-    
+    static get scopedElements() { return {} }
 }
